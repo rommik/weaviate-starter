67f601a68a37f5402ebc08815331abb502206e7e
Roman Mikhailov <rommik@gmail.com>
2023-11-14 04:29:04 +0000

    added poetry and initialized the basic weaviate setup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import weaviate
 import os
 from dotenv import load_dotenv
+import json
+load_dotenv()
 # Only if authentication enabled; assuming API key authentication
 auth_config = weaviate.AuthApiKey(api_key=os.getenv('WV8_API_KEY'))  # Replace w/ your Weaviate instance API key
 
@@ -8,8 +10,10 @@ auth_config = weaviate.AuthApiKey(api_key=os.getenv('WV8_API_KEY'))  # Replace w
 client = weaviate.Client(
     url=os.getenv('WV8_CLUSTER_URI'),
     auth_client_secret=auth_config,  # Only necessary if authentication enabled
-    additional_headers={
-        
+    additional_headers={        
         "X-OpenAI-Api-Key": os.getenv('OPENAI_API_KEY'),            # Replace with your OpenAI key
     }
 )
+
+meta_info = client.get_meta()
+print(json.dumps(meta_info, indent=2))
